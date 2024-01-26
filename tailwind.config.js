e039d9e4bba9e279fd61cdcd61c71a4d2a7819c0
jhohannesK <jdomeh77@gmail.com>
2024-01-26 11:16:56 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#00928F',
+				ligherTone: '#B6DBCA',
+				accent: '#a89060',
+				neutral: '#ffffff',
+				dark: '#02333a',
+			},
+			fontFamily: {
+				Cairo: ["'Cairo'", 'sans-serif'],
+				dancing: ["'Dancing Script'", 'cursive'],
+				kanit: ["'Kanit'", 'sans-serif'],
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 100 },
+				},
+			},
+			animation: {
+				fadeIn: 'fadeIn 0.2s ease-in-out forwards',
+			},
+		},
+	},
+	plugins: [],
+};
